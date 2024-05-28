069ed0ee8ae5af8a1d26569289bfb71f5913867f
sirishin <shinhee4364@gmail.com>
2024-05-28 23:40:27 +0900

    Update lunch.py

--- lunch.py
+++ lunch.py
@@ -3,45 +3,54 @@ import requests
 import datetime
 import re
 now= datetime.datetime.now()
-tomorrow = now + datetime.timedelta(days=1)
-url = 'https://hyoyang.goeic.kr/meal/view.do?menuId=9562&year=%s&month=%s&day=%s' %(now.year,now.month,now.day)
-tourl = 'https://hyoyang.goeic.kr/meal/view.do?menuId=9562&year=%s&month=%s&day=%s' %(tomorrow.year,tomorrow.month,tomorrow.day)
-print(url)
 pattern = r'\([^)]*\)'
 def lunchs():
-    global now
-    if now != datetime.datetime.now():
-        now = datetime.datetime.now()
-        url = 'https://hyoyang.goeic.kr/meal/view.do?menuId=9562&year=%s&month=%s&day=%s' %(now.year,now.month,now.day)
-    req = requests.get(url, verify=False)
-    html = req.text
-    soup = BeautifulSoup(html, 'html.parser')
-    if soup.select('td')[0].get_text().strip() == '식단이 없습니다.':
-        return '오늘 급식은 없습니다'
-    else:
-        a = soup.select('td')[2]
-        print(a)
-        a = a.select('span')[0].get_text().strip()
-        print(a)
-        # a = str.replace("\"", "")
-        z = re.sub(pattern=pattern, repl='', string=a)
-        oa = z.split('ㆍ')
-        print(oa)
-        return oa
+    b=1
+    di = {}
+    while b <31:
+        url = 'https://hyoyang.goeic.kr/meal/view.do?menuId=9562&year=%s&month=%s&day=%s' % (now.year, now.month, b)
+        req = requests.get(url, verify=False)
+        html = req.text
+        soup = BeautifulSoup(html, 'html.parser')
+        if soup.select('td')[0].get_text().strip() == '식단이 없습니다.':
+            di[b] = '오늘 중식은 없습니다'
+        else:
+            a = soup.select('td')[2]
+            # print(a)
+            a = a.select('span')[0].get_text().strip()
+            # print("dfs"+a)
+            # a = str.replace("\"", "")
+            z = re.sub(pattern=pattern, repl='', string=a)
+            oa = z.split('ㆍ')
+            del oa[0]
+            di[b] = oa
+        b=b+1
+    print(di)
+    return di
 def tolunchs():
-    req = requests.get(tourl, verify=False)
-    html = req.text
-    soup = BeautifulSoup(html, 'html.parser')
-    if soup.select('td')[0].get_text().strip() == '식단이 없습니다.':
-        return '내일 급식은 없습니다'
-    else:
-        tolu = soup.select('td')[2]
-        print(tolu)
-        tolu = tolu.select('span')[0].get_text().strip()
-        print(tolu)
-        # tolu = tolu\
-        #     .replace("\"", "")
-        z = re.sub(pattern=pattern, repl='', string=tolu)
-        tolu = z.split('ㆍ')
-        print(tolu)
-        return tolu
+    b = 1
+    di = {}
+    while b < 31:
+        url = 'https://hyoyang.goeic.kr/meal/view.do?menuId=9562&year=%s&month=%s&day=%s' % (now.year, now.month, b)
+        req = requests.get(url, verify=False)
+        html = req.text
+        soup = BeautifulSoup(html, 'html.parser')
+        try:
+            if soup.select('td')[6].get_text().strip() == '식단이 없습니다.':
+                di[b] = '오늘 석식은 없습니다'
+            elif soup.select('td')[6] =='':
+                'd'
+            else:
+                a = soup.select('td')[6]
+                # print(a)
+                a = a.select('span')[0].get_text().strip()
+                # print("dfs" + a)
+                # a = str.replace("\"", "")
+                z = re.sub(pattern=pattern, repl='', string=a)
+                oa = z.split('ㆍ')
+                del oa[0]
+                di[b] = oa
+        except:
+            pass
+        b = b + 1
+    return di

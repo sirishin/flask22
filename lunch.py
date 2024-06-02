a869c8b0d4ac16c7bf48ace93d7958bcea85354a
sirishin <shinhee4364@gmail.com>
2024-06-02 11:29:04 +0900

    Update lunch.py

--- lunch.py
+++ lunch.py
@@ -4,55 +4,33 @@ import datetime
 import re
 now= datetime.datetime.now()
 pattern = r'\([^)]*\)'
-def lunchs():
-    # b=1
-    # di = {}
-    # while b <31:
-    #     url = 'https://hyoyang.goeic.kr/meal/view.do?menuId=9562&year=%s&month=%s&day=%s' % (now.year, now.month, b)
-    #     req = requests.get(url, verify=False)
-    #     html = req.text
-    #     soup = BeautifulSoup(html, 'html.parser')
-    #     if soup.select('td')[0].get_text().strip() == '식단이 없습니다.':
-    #         di[b] = '오늘 중식은 없습니다'
-    #     else:
-    #         a = soup.select('td')[2]
-    #         print(a)
-    #         a = a.select('span')[0].get_text().strip()
-    #         # print("dfs"+a)
-    #         # a = str.replace("\"", "")
-    #         z = re.sub(pattern=pattern, repl='', string=a)
-    #         oa = z.split('ㆍ')
-    #         del oa[0]
-    #         di[b] = oa
-    #     b=b+1
-    # print(di)
-    pass
-    # return di
-def tolunchs():
-    # b = 1
-    # di = {}
-    # while b < 31:
-    #     url = 'https://hyoyang.goeic.kr/meal/view.do?menuId=9562&year=%s&month=%s&day=%s' % (now.year, now.month, b)
-    #     req = requests.get(url, verify=False)
-    #     html = req.text
-    #     soup = BeautifulSoup(html, 'html.parser')
-    #     try:
-    #         if soup.select('td')[6].get_text().strip() == '식단이 없습니다.':
-    #             di[b] = '오늘 석식은 없습니다'
-    #         elif soup.select('td')[6] =='':
-    #             'd'
-    #         else:
-    #             a = soup.select('td')[6]
-    #             # print(a)
-    #             a = a.select('span')[0].get_text().strip()
-    #             # print("dfs" + a)
-    #             # a = str.replace("\"", "")
-    #             z = re.sub(pattern=pattern, repl='', string=a)
-    #             oa = z.split('ㆍ')
-    #             del oa[0]
-    #             di[b] = oa
-    #     except:
-    #         pass
-    #     b = b + 1
-    # return di
-    pass
+di = {}
+url = 'https://hyoyang.goeic.kr/meal/calendar.do?menuId=9562&year=%s&month=%s' % (now.year, now.month)
+req = requests.get(url, verify=False)
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
+for a in soup.select('div[class=dayBox]'):
+    # a.get_text().strip()
+    # cleanr = re.compile('<.*?>')
+    # cleantext = re.sub(cleanr, '', str(a))
+    ass = a.select('div[class=lunch] a')
+    if not ass:
+        dayt = a.select('div[class=dayBox] span')
+        dayt = str(dayt).replace('[<span>','')
+        dayt = str(dayt).replace('</span>]','')
+        # print(str(dayt))
+        di[dayt] = 'its empty'
+        # print()
+    else:
+        oa = str(ass).split('ㆍ')
+        del oa[0]
+        k = oa[-1].split('"')
+        del k[1]
+        oa[-1] = k[0]
+        print(oa)
+        dayt = a.select('div[class=dayBox] span')
+        dayt = str(dayt).replace('[<span>','')
+        dayt = str(dayt).replace('</span>]','')
+        # print(str(dayt))
+        di[dayt] = oa
+print(di)
